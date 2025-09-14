8c643e43aabb2a01ac9bfb3a219a2d5b1b612916
Aaron Mayor <117382876+eonnui@users.noreply.github.com>
2025-09-14 23:53:50 +0800

    Add files via upload

---
+++ Activity1/Activity1.ino
@@ -0,0 +1,17 @@
+int leds[] = {8,9,10,11,12,};
+
+void setup(){
+    for (int i = 0; i < 5; i++){
+      pinMode(leds[i], OUTPUT);
+  }
+}
+
+void loop(){
+  for (int i = 0; i < 5; i++){
+    digitalWrite(leds[i], HIGH);
+    delay(500);
+    digitalWrite(leds[i], LOW);
+    delay(500);
+  }
+
+}
