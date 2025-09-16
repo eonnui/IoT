c6c63d65fabac7889d68bd245dfa7a1910e89a0b
Aaron Mayor <117382876+eonnui@users.noreply.github.com>
2025-09-16 20:44:51 +0800

    Update Lab-Activity1_Working with Digital Signals.ino

--- Activity1/Lab-Activity1_Working with Digital Signals.ino
+++ Activity1/Lab-Activity1_Working with Digital Signals.ino
@@ -10,6 +10,9 @@ void loop(){
   for (int i = 0; i < 5; i++){
     digitalWrite(leds[i], HIGH);
     delay(1000);
+  }
+  
+   for (int i = 0; i < 5; i++){
     digitalWrite(leds[i], LOW);
     delay(1000);
   }
